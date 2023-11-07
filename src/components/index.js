593c38d86973f571aeebbfa5d9b366fc81299681
Chinecherem Harry <chinecherempharry@gmail.com>
2023-11-07 16:16:30 +0100

    working on popular products section

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Nav from "./Nav";
 import Button from "./Button";
 import ShoeCard from "./ShoeCard";
-// import PopularProductCard from "./PopularProductCard";
+import PopularProductCard from "./PopularProductCard";
 // import ServiceCard from "./ServiceCard";
 // import ReviewCard from "./ReviewCard";
 
@@ -9,7 +9,7 @@ export {
   Nav,
   Button,
   ShoeCard,
-  // PopularProductCard,
+  PopularProductCard,
   // ServiceCard,
   // ReviewCard,
 };
