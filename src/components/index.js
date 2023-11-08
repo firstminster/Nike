a80327a9d10f8bc13583e034f9f9c84b9a489e33
Chinecherem Harry <chinecherempharry@gmail.com>
2023-11-08 02:35:08 +0100

    implemented the review card and subscribe section

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,6 @@ import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
-// import ReviewCard from "./ReviewCard";
+import ReviewCard from "./ReviewCard";
 
-export {
-  Nav,
-  Button,
-  ShoeCard,
-  PopularProductCard,
-  ServiceCard,
-  // ReviewCard,
-};
+export { Nav, Button, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
